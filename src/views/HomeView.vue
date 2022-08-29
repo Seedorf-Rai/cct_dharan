4199a97d460c35c617bf40faf764af1840f3b9c5
Seedorf-Rai <dorf123see@gmail.com>
2022-08-29 18:47:23 +0545

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <template-view>
+    <h1>Home Page</h1>
+  </template-view>
+</template>
+
+<script>
+  import TemplateView from './TemplateView.vue'
+export default {
+   name: 'HomeView',
+   components: {TemplateView}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/TemplateView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- Header Section with NavBar -->
+     <header></header>
+    <!-- Header Section with NavBar Ends-->
+
+    <!-- Content Here -->
+    <slot>
+
+    </slot>
+    <!-- Content Ends -->
+
+    <!-- Footer Section Goes Here -->
+    <footer>
+        
+    </footer>
+    <!-- Footer Section Ends -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
